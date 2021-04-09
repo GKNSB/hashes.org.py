8ffeaf10901ed0885dd7e6db5fd7832ec7985aad
GKNSB <manoshatzis@gmail.com>
2021-04-09 21:55:58 +0300

    Fix fix fix

--- hashes.org.py
+++ hashes.org.py
@@ -9,20 +9,26 @@ import tqdm
 
 
 def createSession():
-	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Referer": "https://temp.hashes.org/", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
+	headers = {
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.74 Safari/537.36 Edg/79.0.309.43",
+		"Referer": "https://hashes.org/"
+	}
+
 	session = requests.Session()
 	session.headers.update(headers)
 	return session
 
 
 def queryLeaks(session):
-	response = session.get("https://temp.hashes.org/api/data.php?select=leaks")
-	leaks = json.loads(response.content.strip().decode('utf-8'))
+	response = session.get("https://hashes.org/api/data.php?select=leaks")
+	leaks = json.loads(response.content.strip())
+	response = session.get("https://hashes.org/api/data.php?select=lists")
+	leaks += json.loads(response.content.strip())
 	return leaks
 
 
 def fetchLeak(session, leak):
-	response = session.get("https://temp.hashes.org/download.php?hashlistId={0}&type=found".format(leak["id"]), stream=True)
+	response = session.get("https://hashes.org/download.php?hashlistId={0}&type=found".format(leak["id"]), stream=True)
 
 	with open("{0}.leak".format(leak["id"]), "wb") as leakFile:
 		for chunk in response.iter_content(chunk_size=1024):
@@ -38,30 +44,27 @@ def diff(oldFile, newFile, outFile):
 	os.system("comm -13 {0} {1} > {2}".format(oldFile, newFile, outFile))
 
 
-def addOldUnique(oldFile, newFile):
-	os.system("comm -23 {0} {1} > tempFile".format(oldFile, newFile))
-	os.system("cat tempFile >> {0}".format(newFile))
-	os.remove("tempFile")
-
-
 def main():
 	session = createSession()
 	leaks = queryLeaks(session)
-	print("Fetching {0} leak files...\r\n".format(len(leaks)))
-
 	padding = 0
 
 	for leak in leaks:
-		name = html.unescape(leak["name"].split("<code>")[0])
+		if int(leak["found"].replace("'", "")):
+			info = "{0} [{1}]".format(html.unescape(leak["name"].split("<br><code>")[0]), leak["updated"].replace("<br>", " "))
 
-		if len(name) > padding:
-			padding = len(name)
+			if len(info) > padding:
+				padding = len(info)
 
+		else:
+			leaks.remove(leak)
+
+	print("Fetching {0} leak files...\r\n".format(len(leaks)))
 	tikioudiem = tqdm.tqdm(leaks, miniters=1, bar_format="{desc}{n_fmt: >4}/{total_fmt} {percentage:3.0f}%|{bar}|")
 
 	for item in tikioudiem:
-		name = html.unescape(item["name"].split("<br><code>")[0])
-		tikioudiem.set_description(name + "." * (padding - len(name)))
+		info = "{0} [{1}]".format(html.unescape(item["name"].split("<br><code>")[0]), item["updated"].replace("<br>", " "))
+		tikioudiem.set_description(info + "." * (padding - len(info)))
 		fetchLeak(session, item)
 		sortUnique("{0}.leak".format(item["id"]), "merged.leak")
 		os.remove("{0}.leak".format(item["id"]))
@@ -70,20 +73,12 @@ def main():
 		print("\r\nFound file 'wordlist.txt'!")
 		os.rename("wordlist.txt", "wordlist.old")
 		print("Moved to 'wordlist.old'.")
-
 		print("Deduplicating the merged leak file...")
 		sortUnique("merged.leak", "wordlist.txt")
 		os.remove("merged.leak")
-
-		# print("Adding old unique lines to new wordlist...")
-		# addOldUnique("wordlist.old", "tempFile.txt")
-		# sortUnique("tempFile.txt", "wordlist.txt")
-		# os.remove("tempFile.txt")
-
 		print("Comparing files for new entries...")
 		diff("wordlist.old", "wordlist.txt", "diff.txt")
 		os.remove("wordlist.old")
-
 		print("Done!")
 
 	else:
